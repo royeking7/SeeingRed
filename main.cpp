eacb373bc28cfc9017a80ef7025c177891c38524
royeking7 <rking27@mail.csuchico.edu>
2018-10-22 18:06:46 -0700

    bad image removed

--- main.cpp
+++ main.cpp
@@ -9,46 +9,46 @@ using namespace std;
 int main(int argc, char* argv[])
 
 {
-string filename;
-Bitmap image;
+  string filename;
+  Bitmap image;
 
-if(argc!=2)
-{
-cout<<"Please specify one image file!\n";
-}
-
-if(argc==2)
-{ 
-filename = argv[1];
-image.open(filename);
-}
-
-if(image.isImage()== false)
-{
-cout<<"Image file must be a bitmap with 24-bit color depth.\n";
-}
-
-if(image.isImage()== true)
-{
-vector <vector<Pixel> >imagePixels = image.toPixelMatrix();
-cout<<filename<<" is "<<imagePixels.size()<<" pixel high and "<<imagePixels[0].size()<<" wide "<<endl;
-
-for (int row=0; row<imagePixels.size(); row++)
-{
-  for (int column=0; column<imagePixels[0].size(); column++)
+  if(argc!=2)
   {
-  Pixel color; 
-  color = imagePixels[row][column];
-  color.green=0;
-  color.blue=0;
-  imagePixels[row][column]=color;
+    cout<<"Please specify one image file!\n";
   }
-}
-image.fromPixelMatrix(imagePixels);
-image.save("redness.bmp");
-  
+
+  if(argc==2)
+  { 
+    filename = argv[1];
+    image.open(filename);
+  }
+
+  if(image.isImage()== false)
+  {
+    cout<<"Image file must be a bitmap with 24-bit color depth.\n";
+  }
+
+  if(image.isImage()== true)
+  {
+    vector <vector<Pixel> >imagePixels = image.toPixelMatrix();
+    cout<<filename<<" is "<<imagePixels.size()<<" pixel high and "<<imagePixels[0].size()<<" wide "<<endl;
+
+    for (int row=0; row<imagePixels.size(); row++)
+    {
+      for (int column=0; column<imagePixels[0].size(); column++)
+      {
+        Pixel color; 
+        color = imagePixels[row][column];
+        color.green=0;
+        color.blue=0;
+        imagePixels[row][column]=color;
+      }
+    }
+    image.fromPixelMatrix(imagePixels);
+    image.save("redness.bmp");
 
 
-}
-return 0;
+
+  }
+  return 0;
 }
